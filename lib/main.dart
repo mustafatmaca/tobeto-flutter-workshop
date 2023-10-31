3f253b91d6f99db45665769309930dbbe8f4aa74
mustafatmaca <mustafaatmaca362@gmail.com>
2023-10-31 12:20:28 +0300

    workshop-1

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 3, 104, 48),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/ma.jpg',
+                height: 250,
+                width: 250,
+              ),
+              Text(
+                "Mustafa Atmaca",
+                style: TextStyle(fontSize: 35, color: Colors.white),
+              ),
+              Text(
+                "Mobil Geliştirici Flutter - 1A",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+              Text(
+                "31/10/2023",
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
