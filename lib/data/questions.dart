237e769d84c2e4e49ce9b7c686ef56bf45050ff8
mustafatmaca <mustafaatmaca362@gmail.com>
2023-11-02 12:04:23 +0300

    workshop 2/11/23

---
+++ lib/data/questions.dart
@@ -0,0 +1,13 @@
+import 'package:intro/model/quiz_question.dart';
+
+const questions = [
+  QuizQuestion("What is Flutter?",
+      ["Programming Language", "Framework", "Database", "Game Engine"]),
+  QuizQuestion("Which one we use for update screen?",
+      ["setState", "main", "constructor", "String"]),
+  QuizQuestion("What is Dart?",
+      ["Programming Language", "Framework", "Database", "Game Engine"]),
+  QuizQuestion(
+      "What is the file where we add the file path when we want to add an image?",
+      ["pubspec.yaml", "lib", "main.dart", "README.md"]),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:intro/model/quiz_question.dart';
+import 'package:intro/data/questions.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 3, 104, 48),
+    const MaterialApp(
+      home: QuestionScreen(),
+    ),
+  );
+}
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/ma.jpg',
-                height: 250,
-                width: 250,
+              Image.asset('assets/quiz.jpeg'),
+              const Text(
+                "Quiz App",
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Text(
-                "Mustafa Atmaca",
-                style: TextStyle(fontSize: 35, color: Colors.white),
-              ),
-              Text(
-                "Mobil Geliştirici Flutter - 1A",
-                style: TextStyle(fontSize: 25, color: Colors.white),
-              ),
-              Text(
-                "31/10/2023",
-                style: TextStyle(fontSize: 15, color: Colors.white),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_forward_ios),
+                label: const Text("Start"),
+                style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black87,
+                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 20)),
               ),
             ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({Key? key}) : super(key: key);
+
+  @override
+  _QuestionState createState() => _QuestionState();
+}
+
+class _QuestionState extends State<QuestionScreen> {
+  int questionNumber = 0;
+
+  void changeQuestion() {
+    setState(() {
+      questionNumber++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(questions[questionNumber].question),
+            ...questions[questionNumber].answers.map((answer) {
+              return ElevatedButton(
+                onPressed: () {
+                  if (questionNumber < 3) {
+                    changeQuestion();
+                  }
+                },
+                child: Text(answer),
+              );
+            })
+          ],
+        ),
+      ),
+    );
+  }
 }
